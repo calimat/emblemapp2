337a5930c3ecfe0a773e91edfad4afefea0d5642
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-08-01 18:00:44 -0500

    Initial Commit

---
+++ Emblater2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Emblater2
+//
+//  Created by Ricardo Herrera Petit on 8/1/20.
+//  Copyright © 2020 Ricardo Herrera Petit. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
